fc7b778e178c932a557a479d41e5f0aaa641a45b
Eugene <eugene.kolesnikov.95@gmail.com>
2016-05-10 01:05:15 +0300

    Updated files.

--- nbody.frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 410 core
-
-in vec4 position;
-out vec4 fColor;
-
-void main (void) {
-    fColor = vec4(0,0,1,1);
-}
--- shaders/potential.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 410 core
-
-in vec3 color;
-out vec4 fColor;
-
-void main (void) {
-    fColor = vec4(color.xyz,1);
-    //fColor = vec4(1,1,1,1);
-}
